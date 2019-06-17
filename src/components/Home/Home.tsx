5aa8918acf68e3144162cb41748a38072830bef1
RJ Dioneda <dionedarj@gmail.com>
2019-06-16 22:35:17 -0700

    Added back in MTG specific code.

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -3,6 +3,8 @@ import React, { useState } from "react";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 import { makeStyles } from "@material-ui/styles";
+import fs from "fs";
+import cheerio from "cheerio";
 
 const { remote } = require("electron");
 
@@ -20,11 +22,42 @@ type HomeProps = {
     home: string
 };
 
+const openSaveDialog = (content) => () => {
+    dialog.showSaveDialog(remote.getCurrentWindow(), {}, (fileName: string) => {
+        if (!fileName) {
+            console.error("No file was saved.");
+            return;
+        }
+
+        fs.writeFile(fileName, content, (err => {
+            if (err) {
+                alert(`An error occured ${err.message}`);
+            } else {
+                alert("File successfully saved.");
+            }
+        }));
+    });
+};
+
+const makeRequest = (url: string) => async () => {
+    try {
+        const httpUrl = url.startsWith("http") ? url : `http://${url}`;
+        const request = await fetch(httpUrl);
+        if (request.ok) {
+            const text = await request.text();
+            const $ = cheerio.load(text);
+            console.log($("#mtga-textarea").text());
+        }
+    } catch (error) {
+        alert(`An error occured ${error.message}`);
+    }
+};
 
 const handleChange = setFunction => event => setFunction(event.target.value);
 
 const Home = ({ setHome, home }: HomeProps) => {
     const [text, setText] = useState("");
+    const [tappedOutUrl, setTappedOutUrl] = useState("");
     const classes = styles();
 
     return (
@@ -39,6 +72,17 @@ const Home = ({ setHome, home }: HomeProps) => {
             <div>
                 <Button variant="outlined" onClick={() => setHome(text)} >
                     Hello worlds!
+                </Button>
+            </div>
+            <div>
+                <Button variant="outlined" onClick={openSaveDialog(text)} >
+                    Save Random File In Text?
+                </Button>
+            </div>
+            <div>
+                <TextField value={tappedOutUrl} onChange={handleChange(setTappedOutUrl)} />
+                <Button variant="outlined" onClick={makeRequest(tappedOutUrl)} >
+                    Make dek rekwest
                 </Button>
             </div>
         </div>
