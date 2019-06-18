72767affc830cb5fd94712fff12d207cc961d014
RJ Dioneda <dionedarj@gmail.com>
2019-06-17 23:13:24 -0700

    Updated README and added decklist text field.

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -14,6 +14,13 @@ const styles = makeStyles({
     container: {
         top: "30%",
         textAlign: "center"
+    },
+    deckList: {
+        height: 800,
+        width: 500
+    },
+    tappedOutUrl: {
+        width: 500
     }
 });
 
@@ -39,14 +46,14 @@ const openSaveDialog = (content) => () => {
     });
 };
 
-const makeRequest = (url: string) => async () => {
+const makeRequest = (url: string, setDeckList: (list: string) => void) => async () => {
     try {
         const httpUrl = url.startsWith("http") ? url : `http://${url}`;
         const request = await fetch(httpUrl);
         if (request.ok) {
             const text = await request.text();
             const $ = cheerio.load(text);
-            console.log($("#mtga-textarea").text());
+            setDeckList($("#mtga-textarea").text());
         }
     } catch (error) {
         alert(`An error occured ${error.message}`);
@@ -58,12 +65,13 @@ const handleChange = setFunction => event => setFunction(event.target.value);
 const Home = ({ setHome, home }: HomeProps) => {
     const [text, setText] = useState("");
     const [tappedOutUrl, setTappedOutUrl] = useState("");
+    const [deckList, setDeckList] = useState("");
     const classes = styles();
 
     return (
         <div className={classes.container} data-tid="container">
             <h2>Home</h2>
-            <div>
+            {/* <div>
                 <TextField value={text} onChange={handleChange(setText)} />
             </div>
             <div>
@@ -78,12 +86,32 @@ const Home = ({ setHome, home }: HomeProps) => {
                 <Button variant="outlined" onClick={openSaveDialog(text)} >
                     Save Random File In Text?
                 </Button>
-            </div>
+            </div> */}
             <div>
-                <TextField value={tappedOutUrl} onChange={handleChange(setTappedOutUrl)} />
-                <Button variant="outlined" onClick={makeRequest(tappedOutUrl)} >
-                    Make dek rekwest
-                </Button>
+                <TextField
+                    label="Tappedout Deck URL"
+                    value={tappedOutUrl}
+                    onChange={handleChange(setTappedOutUrl)}
+                    InputProps={{
+                        className: classes.tappedOutUrl
+                    }}
+                />
+            </div>
+            <Button variant="outlined" onClick={makeRequest(tappedOutUrl, setDeckList)} >
+                    Fetch Deck List
+            </Button>
+            <div>
+                <TextField
+                    variant="outlined"
+                    value={deckList}
+                    multiline
+                    rows={40}
+                    label="Deck List"
+                    margin="normal"
+                    onChange={handleChange(setDeckList)}
+                    InputProps={{
+                        className: classes.deckList
+                    }}/>
             </div>
         </div>
     );
